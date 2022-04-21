b787e6d8d78ed31abf5c51b4d6c1ada5dabdd56c
ajitjakasania <ajit.jakasania@gmail.com>
2022-04-21 10:28:33 -0700

    Added movie rental base code

---
+++ README.md
@@ -0,0 +1,4 @@
+Movie Rental Application
+
+Using official mongo docker image for mongo setup.
+https://hub.docker.com/_/mongo?tab=description
---
+++ convert_json.py
@@ -0,0 +1,62 @@
+import csv
+import json
+from tokenize import Double
+ 
+# Function to convert a CSV to JSON
+# Takes the file paths as arguments
+def make_json(csvFilePath, jsonFilePath):
+     
+    # create a dictionary
+    data = []
+     
+    # Open a csv reader called DictReader
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+         
+        # Convert each row into a dictionary
+        # and add it to data
+        for rows in csvReader:
+             
+            # Assuming a column named 'No' to
+            # be the primary key
+            # key = rows['id']
+            #price
+            price = int (rows["price"])
+            rows["price"] = price
+            
+            #runtime
+            runtime = int (rows["runtime"])
+            rows["runtime"] = runtime
+
+            #popularity
+            popularity = float (rows["popularity"])
+            rows["popularity"] = popularity
+            
+            #vote_average
+            vote_average = float (rows["vote_average"])
+            rows["vote_average"] = vote_average
+
+            #amount_copies
+            amount_copies = int (rows["amount_copies"])
+            rows["amount_copies"] = amount_copies
+
+            #vote_count
+            vote_count = int (rows["vote_count"])
+            rows["vote_count"] = vote_count
+
+            data.append(rows)
+ 
+    # Open a json writer, and use the json.dumps()
+    # function to dump data
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+         
+# Driver Code
+ 
+# Decide the two file paths according to your
+# computer system
+csvFilePath = r'/Users/ajakasan/Desktop/My Stuff/Class/CS 157C/Project/movies_metadata.csv'
+jsonFilePath = r'/Users/ajakasan/Desktop/My Stuff/Class/CS 157C/Project/test.json'
+ 
+# Call the make_json function
+make_json(csvFilePath, jsonFilePath)
